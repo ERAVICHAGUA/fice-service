8d2be3672af2623abd4557e219ac017fb1e2af93
Elias Ravichagua <egrp21405@gmail.com>
2026-03-11 01:18:23 -0500

    Hora Preterminada

--- src/main/java/com/engineai/fice/FiceServiceApplication.java
+++ src/main/java/com/engineai/fice/FiceServiceApplication.java
@@ -3,11 +3,17 @@ package com.engineai.fice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FiceServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiceServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        // 👇 ESTA LINEA ARREGLA LA HORA
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+
+        SpringApplication.run(FiceServiceApplication.class, args);
+    }
 
 }
